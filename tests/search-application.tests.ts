7ccf4baa735d593e3d5d69bf857cc85b467a425b
Jakub Czyż <84026082+JakubC-projects@users.noreply.github.com>
2021-09-21 10:48:21 +0200

    Search-for-application (#24)
    * add search for applictaion
    
    * update version to 3.1.7
    
    * add application search test
    
    * change package version
    
    * remove .only

---
+++ tests/search-application.tests.ts
@@ -0,0 +1,40 @@
+import feathers from "@feathersjs/feathers";
+import { Application } from "@feathersjs/feathers";
+import { expect } from "chai";
+import ArangoDbService, { IArangoDbService, AUTH_TYPES } from "../src";
+import { importDB } from "./setup-tests/setup";
+
+let testUserWithARole: any = null;
+
+describe.skip(`Search tests on the application service `, () => {
+  let app: Application<any>;
+  let service: IArangoDbService<any>;
+
+  before(async () => {
+    await importDB()
+    app = feathers();
+    app.use(
+      `/application`,
+      ArangoDbService({
+        collection: 'application',
+        view: 'application_view',
+        database: "TEST",
+        authType: AUTH_TYPES.BASIC_AUTH,
+        username: "root",
+        password: "root",
+        dbConfig: {
+          url: "http://localhost:8529",
+        }
+      })
+    );
+    service = <IArangoDbService<any>>app.service('application');
+    //testUserWithARole = await service.get('178509735', {});
+
+  });
+
+  it("Search - application", async () => {
+    const results = await service.find({ query: { $search: 'Samvirk' } });
+    expect(results[0]._id).to.eq("application/522409136");
+  });
+
+});
