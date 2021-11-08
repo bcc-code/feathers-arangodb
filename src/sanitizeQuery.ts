d6d3346b61b9fb4229367ac6e86b4d150b2d317f
Jakub Czyz <jakub12134@gmail.com>
2021-11-08 14:46:02 +0100

    remove sanitizeQuery.ts

--- src/sanitizeQuery.ts
+++
@@ -1,7 +0,0 @@
-export default function sanitizeFieldName(fieldName: string) {
-  const tempValue = fieldName.replace(/[^a-zA-Z0-9_\[\]\.]+/g, ''); //this removes all characters except alphanumeric ones and _ [ ] and .(dot). 
-  if (tempValue !== fieldName) {
-      console.warn(`String was sanitized, input: ${fieldName}, output: ${tempValue}`);
-  }
-  return tempValue;
-}
