d400ed5c68cf96ab8113143641da16598af7c9e7
Mikołaj Kubista <mikipage@gmail.com>
2022-01-11 18:30:08 +0100

    FinalLogger (#35)
    * bumped version
    
    * fixed logger
    
    Co-authored-by: mikolaj.kubista <mikolaj.kubista@gmail.com>

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,18 @@
 import {createLogger, format, transports} from 'winston';
 import {LoggingWinston} from '@google-cloud/logging-winston';
 
-const isLocalEnvironment = process.env.GCP_PROJECT ? false : true;
-const projectId = isLocalEnvironment ? 'localhost' : process.env.GCP_PROJECT;
-const serviceName = isLocalEnvironment ? 'localhost' : process.env.K_SERVICE;
+const isLocalEnvironment = process.env.K_SERVICE ? false : true;
+const serviceName = process.env.K_SERVICE ? 'localhost' : process.env.K_SERVICE;
 
 const loggingWinston = new LoggingWinston({
     logName: `${serviceName}-FeathersArangoAdapter-logs`,
     serviceContext: {
         service: serviceName,
     },
-    resource: {
-        type: 'cloud_run_revision',
-        labels: {
-            project_id: projectId,
-            service_name: serviceName,
-            configuration_name: serviceName,
-        } as {},
-    },
+    labels: {
+        component: 'FeathersArangoAdapter',
+        service: serviceName,
+    } as {},
 });
 
 const logger = createLogger({
@@ -26,5 +21,4 @@ const logger = createLogger({
     transports: isLocalEnvironment ? [new transports.Console()] : [loggingWinston],
 });
 
-logger.debug('Feathers-Arangodb adapter logger initialized', {isLocal: isLocalEnvironment, projectId, serviceName, gcp: process.env.GCP_PROJECT, envVars: process.env});
 export default logger;
